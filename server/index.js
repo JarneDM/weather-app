584c8303eab1c3518ef2f64b259208881fd74c4d
JarneDM <jarne.demeyer05@hotmail.com>
2025-10-06 12:52:42 +0200

    hopefully fixed fetching now, works on my localhost but not vercel

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ import favoriteController from "./controllers/favoriteController.js";
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://weather-app-jarnedm.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/api/weather/:city", weatherController.getCityWeather);
 app.get("/api/favorites", favoriteController.getFavorites);
